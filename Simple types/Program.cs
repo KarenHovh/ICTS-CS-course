4678506c6662878d3ca4f59c25de52ab422f8537
KarenHov <KarenHovh>
2018-09-26 20:21:45 +0400

    Hi Everybody!

---
+++ Simple types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int y = 6;
+            Console.WriteLine(x + y);
+
+            Console.ReadKey();
+        }
+    }
+}
