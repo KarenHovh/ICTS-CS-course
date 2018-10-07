878203f4cdd6fe379717ef0c625a5c81cc8d4341
Admin <Admin@ADMIN-PC>
2018-10-07 17:01:22 +0400

    Added  Loop

---
+++ Ex_2/Ex_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = 4;
+            int N = 6;
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(K);
+            }
+            Console.ReadKey();
+        }
+    }
+}
