5ee711a272ff1aae298b66ab14d5f6b3f23b9e59
Admin <Admin@ADMIN-PC>
2018-10-09 22:10:47 +0400

    Added

---
+++ Exercise 41/Ex 42/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, n, p = 1;
+            int dec = 0, i = 1, j, d;
+          
+
+            Console.Write("Input a number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            k = n;
+            for (j = n; j > 0; j = j / 10)
+            {
+                d = j % 10;
+                if (i == 1)
+                    p *= 1;
+                else
+                    p *= 2;
+
+                dec = dec + (d * p);
+                i++;
+            }
+            Console.Write("\nThe Binary Number : {0}\nThe equivalent Decimal  Number : {1} \n\n", k, dec);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise 41/Exercise 41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k, j, binno = 0, sum;
+            
+            Console.Write("Enter a number to convert : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            sum = n;
+            k = 1;
+            for (j = n; j > 0; j = j / 2)
+            {
+                binno = binno + (n % 2) * k;
+                k = k * 10;
+                n = n / 2;
+            }
+
+            Console.Write("\nThe Binary of {0} is {1}.\n\n", sum, binno);
+
+            Console.ReadKey();
+        }
+    }
+}
